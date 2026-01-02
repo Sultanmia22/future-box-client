242fe05335689aae116e5d6f471327531f71214d
Sultan Mia <sultanmia5732@gmail.com>
2026-01-02 17:21:33 +0600

    update navbar drop down menu

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -3,7 +3,9 @@ import { use, useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router';
 import { AuthContext } from '../../Auth/AuthContext';
 import { toast } from 'react-toastify';
-
+import { FaRegUser } from "react-icons/fa";
+import { CiSettings } from 'react-icons/ci';
+import { IoIosLogOut } from 'react-icons/io';
 
 const Navbar = () => {
 
@@ -94,52 +96,52 @@ const Navbar = () => {
           </div>
           {
             user ?
-              <div className='flex items-center gap-3' onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+              <div className='flex items-center gap-3 '>
                 <div>
-                  <figure className='border-2 border-primary rounded-full'>
-                    <img src={user?.photoURL} alt="" className='w-13 h-13 p-1 rounded-full' />
-                  </figure>
+                  <div onClick={() => setShow(!show)} className='relative'>
+                    <figure className='border-2 border-primary rounded-full'>
+                      <img src={user?.photoURL} alt="" className='w-13 h-13 p-1 rounded-full' />
+                    </figure>
+                  </div>
 
                   {
                     show &&
-                    <div className=' absolute bg-base-100 shadow border border-gray-400 px-10 py-2 rounded-lg top-13 right-1 z-10'>
+                    <div className='absolute top-22 md:top-24 right-0 bg-base-100 p-2 md:p-5 rounded-xl w-[300px] shadow-md'>
+                     <div className="photo-section border-2 border-gray-100 py-3 md:py-5 rounded-xl">
+                        <figure>
+                          <img src={user?.photoURL} alt="" className=' w-auto h-auto md:w-20 md:h-20 mx-auto rounded-full'/>
+                        </figure>
 
-                      <div className='mr-3 md:hidden flex justify-center pb-1'>
-                        <label className="swap swap-rotate">
-                          {/* this hidden checkbox controls the state */}
-                          <input onChange={(e) => handleTheme(e.target.checked)} type="checkbox" className="theme-controller" />
+                        <h2 className='text-center text-base text-gray-600 dark:text-white font-medium'>{user?.displayName}</h2>
+                     </div>
 
-                          {/* sun icon */}
-                          <svg
-                            className="swap-off h-10 w-10 fill-current"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24">
-                            <path
-                              d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                          </svg>
+                     <div className='my-2'>
+                      <ul className='space-y-2'>
+                        <Link className='flex items-center gap-2 font-medium '>
+                          <span> <FaRegUser size={18}/></span> 
+                          <span>My Profile</span> 
+                        </Link>
 
-                          {/* moon icon */}
-                          <svg
-                            className="swap-on h-10 w-10 fill-current"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24">
-                            <path
-                              d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                          </svg>
-                        </label>
-                      </div>
+                        <Link className='flex items-center gap-2 font-medium '>
+                          <span> <CiSettings size={18}/></span> 
+                          <span>Settings</span> 
+                        </Link>
 
-                      <p className='pb-2 text-gray-500 dark:text-white font-medium'> {user?.displayName} </p>
-
-                      <button onClick={handleSignOut} className='btn btn-outline border border-secondary text-secondary hover:bg-secondary hover:text-white'> Sign Out </button>
+                         <button onClick={handleSignOut} className='flex items-center gap-2 font-medium cursor-pointer'>
+                          <span className='text-red-500'> <IoIosLogOut size={18}/></span> 
+                          <span>Logout</span> 
+                        </button>
+                      </ul>
+                     </div>
                     </div>
                   }
+
                 </div>
               </div>
               :
               <div className='flex items-center gap-3'>
-                <Link to='/login' className='btn btn-outline   border border-primary text-primary hover:bg-primary hover:text-white'> Sign In </Link>
-                <Link to='/registration' className='btn btn-outline  border border-secondary text-secondary hover:bg-secondary hover:text-white'> Sign UP </Link>
+                <Link to='/login' className='text-primary  font-semibold  '> Sign In </Link>
+                <Link to='/registration' className=' text-secondary font-semibold  '> Sign UP </Link>
               </div>
           }
         </div>
